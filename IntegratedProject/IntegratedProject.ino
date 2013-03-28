f1cde7c55973ff7e7f5181718f2215907c814b1f
Steven Landis <slandis@Stevens-MacBook-Pro.local>
2013-03-28 19:32:48 -0400

    Added ultrasonic code for sorting an array and picking the largest value

--- IntegratedProject/IntegratedProject.ino
+++ IntegratedProject/IntegratedProject.ino
@@ -15,7 +15,8 @@ TxStatusResponse txStatus = TxStatusResponse();
 //SENSORS
 const int sensorIR = 2;
 const int pwPin = 5;
-int arraysize = 1;
+int arraysize = 9;
+int rangevalue[] = { 0, 0, 0 ,0, 0, 0, 0, 0, 0};
 float irValue, irInches, irCm, ultrasonicInches;
 long pulse;
 
@@ -122,17 +123,31 @@ int irSensor(){
 
 int ultrasonicSensor(){
   pinMode(pwPin, INPUT);
-  /*for(int i = 0; i < arraysize; i++)
+  for(int i = 0; i < arraysize; i++)
   {								    
     pulse = pulseIn(pwPin, HIGH);
     rangevalue[i] = pulse/147;
     delay(10);
   }
-  */
-  pulse = pulseIn(pwPin, HIGH);
-  ultrasonicInches = pulse/147;
-  return (int)ultrasonicInches;
+  isort(rangevalue,arraysize);
+  //pulse = pulseIn(pwPin, HIGH);
+  //ultrasonicInches = pulse/147;
+  return (int)rangevalue[8];
   
+}
+
+void isort(int *a, int n){
+// *a is an array pointer function
+  for (int i = 1; i < n; ++i)
+  {
+    int j = a[i];
+    int k;
+    for (k = i - 1; (k >= 0) && (j < a[k]); k--)
+    {
+      a[k + 1] = a[k];
+    }
+    a[k + 1] = j;
+  }
 }
 
 void xbeeSend(){
